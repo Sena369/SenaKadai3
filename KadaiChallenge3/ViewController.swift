a53bbd6df0d98b6f589bae985f266c3ac8db7f98
Sena <senana0116@icloud.com>
2022-03-12 17:19:00 +0900

    リファクタリング

--- KadaiChallenge3/ViewController.swift
+++ KadaiChallenge3/ViewController.swift
@@ -13,9 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBOutlet private weak var firstText: UITextField!
-    @IBOutlet private weak var secondText: UITextField!
+    
+    @IBOutlet private var textField: [UITextField]!
     @IBOutlet private weak var firstSwitch: UISwitch!
     @IBOutlet private weak var secondSwitch: UISwitch!
     @IBOutlet private weak var firstLabel: UILabel!
@@ -24,29 +23,80 @@ class ViewController: UIViewController {
  
     @IBAction func calculateButton(_ sender: Any) {
         
-        let firstValue = Int(firstText.text ?? "") ?? 0
-        let secondValue = Int(secondText.text ?? "") ?? 0
-        
-        let num1 : Int
-        let num2 : Int
-        
-        if firstSwitch.isOn == true {
-            num1 = 0 - firstValue
-        } else {
-            num1 = firstValue
-        }
-        
-        if secondSwitch.isOn == true {
-            num2 = 0 - secondValue
-        } else {
-            num2 = secondValue
-        }
+        let value = textField.map{ Int($0.text ?? "") ?? 0 }
+        let num1 = switchJudge(firstSwitch, value[0])
+        let num2 = switchJudge(secondSwitch, value[1])
         
         firstLabel.text = String(num1)
         secondLabel.text = String(num2)
         
         resultLabel.text = String(num1 + num2)
         
+        func switchJudge(_ symbolSwitch: UISwitch , _ textValue: Int) -> Int {
+        
+            if symbolSwitch.isOn == true {
+                return 0 - textValue
+            } else {
+                return textValue
+            }
+        }
     }
 }
+        
+//        var num : [Int] = []
+//        num = { (symbolSwitch , textValue) ->  in
+//            if symbolSwitch.isOn == true {
+//                num = 0 - textValue
+//            } else {
+//                num = textValue
+//            }
+//        }
+        
+//        num[0] = num(firstSwitch, value[0])
+//        num[1] = num(secondSwitch, value[1])
+        
+//        symbolSwitch.forEach { if $0.isOn == true {
+//        if symbolSwitch[0].isOn == true {
+//            num[0] = 0 - value[0]
+//            num = value.map{ element in (0 - element) }
+//        } else {
+//            num[0] = value[0]
+//        }
+        
+//        if symbolSwitch[1].isOn == true {
+//            num[1] = 0 - value[1]
+//            num = value.map{ element in (0 - element) }
+//        } else {
+//            num[1] = value[1]
+//        }
+//        print(num)
+        
+//        var textLabel = [firstLabel.text , secondLabel.text]
+//        textLabel = num.map { String($0) }
+        
+//        resultLabel.text = String(num.forEach(element in element.sum))
+
+        
+//        let firstValue = Int(firstText.text ?? "") ?? 0
+//        let secondValue = Int(secondText.text ?? "") ?? 0
+                
+//        let num1 : Int
+//        let num2 : Int
+        
+//        if firstSwitch.isOn == true {
+//            num1 = 0 - firstValue
+//        } else {
+//            num1 = firstValue
+//        }
+//
+//        if secondSwitch.isOn == true {
+//            num2 = 0 - secondValue
+//        } else {
+//            num2 = secondValue
+//        }
+        
+       
+//
+                
+
 
