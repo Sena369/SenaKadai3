fbef69105ff633605455494366ca334b8f8e0034
Sena <senana0116@icloud.com>
2022-03-12 18:31:50 +0900

    リファクタリング後

--- KadaiChallenge3/ViewController.swift
+++ KadaiChallenge3/ViewController.swift
@@ -17,43 +17,22 @@ class ViewController: UIViewController {
     @IBOutlet private weak var resultLabel: UILabel!
  
     @IBAction func calculateButton(_ sender: Any) {
-        
-        let textFieldValue : [Int] = textField.map{ Int($0.text ?? "") ?? 0 }
-        firstLabel.text  = String(switchJudge(firstSwitch, textFieldValue[0]))
-        secondLabel.text = String(switchJudge(secondSwitch, textFieldValue[1]))
-        resultLabel.text = String(switchJudge(firstSwitch, textFieldValue[0]) + switchJudge(secondSwitch, textFieldValue[1]))
-        
-        func switchJudge(_ symbolSwitch: UISwitch , _ textValue: Int) -> Int {
-        
-            if symbolSwitch.isOn == true {
-                return 0 - textValue
-            } else {
-                return textValue
-            }
+
+        let textFieldValue = textField.map{ Int($0.text ?? "") ?? 0 }
+        let num : (UISwitch,Int) -> Int = {
+            $0.isOn ?  -$1 : $1
         }
+        
+        let num1 = num(firstSwitch, textFieldValue[0])
+        let num2 = num(secondSwitch, textFieldValue[1])
+        
+        firstLabel.text = String(num1)
+        secondLabel.text = String(num2)
+        resultLabel.text = String(num1 + num2)
     }
 }
         
-//        let firstValue = Int(firstText.text ?? "") ?? 0
-//        let secondValue = Int(secondText.text ?? "") ?? 0
-                
-//        let num1 : Int
-//        let num2 : Int
-        
-//        if firstSwitch.isOn == true {
-//            num1 = 0 - firstValue
-//        } else {
-//            num1 = firstValue
-//        }
-//
-//        if secondSwitch.isOn == true {
-//            num2 = 0 - secondValue
-//        } else {
-//            num2 = secondValue
-//        }
-        
-       
-//
+
                 
 
 
